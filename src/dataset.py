fe8b4a784e172b610b0fda27b6e0e13680cdb0a8
Dong Hwan Kim <72735688+DonHK-97@users.noreply.github.com>
2021-12-02 23:01:20 +0900

    dataset.py (#7)
    LeakyReLU to PReLU
    Added Learning Scheduler
    
    correction of dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -35,7 +35,7 @@ class MCDataset(InMemoryDataset):
 
     @property
     def raw_file_names(self):
-        return ['u.data', 'u1.test']
+        return ['u.data', 'u.test']
 
     @property
     def processed_file_names(self):
@@ -44,21 +44,23 @@ class MCDataset(InMemoryDataset):
     def process(self):
         train_csv, test_csv = self.raw_paths
         train_df, train_nums = self.create_df(train_csv)
+        test_df, test_nums = self.create_df(test_csv)
 
         train_idx, train_gt = self.create_gt_idx(train_df, train_nums)
-
-        train_df['product_id'] = train_df['product_id'] + train_nums['user']
+        test_idx, test_gt = self.create_gt_idx(test_df, train_nums)
+        
+        train_df['access'] = train_df['access'] + train_nums['user']
 
         x = torch.arange(train_nums['node'], dtype=torch.long)
 
         # Prepare edges
         edge_user = torch.tensor(train_df['user_id'].values)
-        edge_item = torch.tensor(train_df['product_id'].values)
+        edge_item = torch.tensor(train_df['access'].values)
         edge_index = torch.stack((torch.cat((edge_user, edge_item), 0),
                                   torch.cat((edge_item, edge_user), 0)), 0)
         edge_index = edge_index.to(torch.long)
 
-        edge_type = torch.tensor(train_df['method'])
+        edge_type = torch.tensor(train_df['product'])
         edge_type = torch.cat((edge_type, edge_type), 0)
 
         edge_norm = copy.deepcopy(edge_index[1])
@@ -76,7 +78,7 @@ class MCDataset(InMemoryDataset):
         data.train_idx = train_idx
         data.train_gt = train_gt
         data.num_users = torch.tensor([train_nums['user']])
-        data.num_items = torch.tensor([train_nums['product']])
+        data.num_items = torch.tensor([train_nums['access']])
         
         data, slices = self.collate([data])
         torch.save((data, slices), self.processed_paths[0])
@@ -85,19 +87,21 @@ class MCDataset(InMemoryDataset):
         col_names = ['user_id', 'rate', 'sentiment', 'polarity', 'product_id', 'device', 'method']
         df = pd.read_csv(csv_path, sep='\t', names=col_names)
         df['rate'] = df['rate'] * df['sentiment']
-        df = df.drop(['sentiment', 'polarity'], axis=1)
-        df['method'] = df['method'] + 1
+        df['access'] = df['device'] + df['method'] - 2
+        df = df.drop(['sentiment', 'polarity', df['device'], df['method']], axis=1)
+        df['user_id'] = df['user_id'] - 1
+        df['product'] = df['product'] - 1
 
-        nums = {'user': df.max()['user_id'],
-                'product': df.max()['product_id'] + 1,
-                'node': df.max()['user_id'] + df.max()['product_id'] + 1,
+        nums = {'user': df.max()['user_id'] + 1,
+                'access' : df.max()['access'] + 1,
+                'node': df.max()['user_id'] + df.max()['access'] + 2,
                 'edge': len(df)}
         return df, nums
 
     def create_gt_idx(self, df, nums):
-        df['idx'] = df['user_id'] * nums['product'] + df['product_id']
+        df['idx'] = df['user_id'] * nums['access'] + df['access'] * df['rate'] 
         idx = torch.tensor(df['idx'])
-        gt = torch.tensor(df['method'])
+        gt = torch.tensor(df['product_id'])
         return idx, gt
 
     def get(self, idx):
@@ -106,7 +110,6 @@ class MCDataset(InMemoryDataset):
 
     def __repr__(self):
         return '{}{}()'.format(self.name.upper(), self.__class__.__name__)
-
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
